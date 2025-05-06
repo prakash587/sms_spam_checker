778317907fd80432598f24790ace1cb452344314
prakash587 <80191742+prakash587@users.noreply.github.com>
2025-05-06 10:56:26 +0545

    added nltk.txt and made some changee on app.py

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 import streamlit as st
 import pickle
 import string
-import nltk
 from nltk.corpus import stopwords
+import nltk
 from nltk.stem.porter import PorterStemmer
-
-# 🔧 Download required NLTK data at runtime
-nltk.download('punkt')
-nltk.download('stopwords')
 
 ps = PorterStemmer()
 
@@ -36,27 +32,21 @@ def transform_text(text):
 
     return " ".join(y)
 
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
 
-# 🔃 Load model and vectorizer
-tfidf = pickle.load(open('vectorizer12.pkl', 'rb'))
-model = pickle.load(open('model12.pkl', 'rb'))
-
-# 🎯 Streamlit UI
 st.title("Email/SMS Spam Classifier")
 
 input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
 
-    # 1. Preprocess
+    # 1. preprocess
     transformed_sms = transform_text(input_sms)
-
-    # 2. Vectorize
+    # 2. vectorize
     vector_input = tfidf.transform([transformed_sms])
-
-    # 3. Predict
+    # 3. predict
     result = model.predict(vector_input)[0]
-
     # 4. Display
     if result == 1:
         st.header("Spam")
