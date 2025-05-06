c7e51b7a365ebe0fd3ef586ebb7c295e7028bf73
prakash587 <80191742+prakash587@users.noreply.github.com>
2025-05-06 12:30:10 +0545

    model, vectorizer file

--- app.py
+++ app.py
@@ -32,8 +32,8 @@ def transform_text(text):
 
     return " ".join(y)
 
-tfidf = pickle.load(open('vectorizer.pkl','rb'))
-model = pickle.load(open('model.pkl','rb'))
+tfidf = pickle.load(open('vectorizer12.pkl','rb'))
+model = pickle.load(open('model12.pkl','rb'))
 
 st.title("Email/SMS Spam Classifier")
 
